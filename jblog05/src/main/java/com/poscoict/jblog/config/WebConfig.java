211809fdde318355b3bed51232b5074ed528ece2
Wonseok <ls4050@naver.com>
2022-02-13 02:59:23 +0900

    jblog05 - blogInterceptor add

--- jblog05/src/main/java/com/poscoict/jblog/config/WebConfig.java
+++ jblog05/src/main/java/com/poscoict/jblog/config/WebConfig.java
@@ -1,20 +1,35 @@
 package com.poscoict.jblog.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.context.annotation.Import;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 import com.poscoict.config.web.AssetsMappingConfig;
 import com.poscoict.config.web.FileuploadConfig;
 import com.poscoict.config.web.MvcConfig;
 import com.poscoict.config.web.SecurityConfig;
+import com.poscoict.jblog.interceptor.BlogInterceptor;
 
 @Configuration
 @EnableAspectJAutoProxy
 @ComponentScan({"com.poscoict.jblog.controller","com.poscoict.jblog.exception"})
 @Import({MvcConfig.class, SecurityConfig.class, FileuploadConfig.class, AssetsMappingConfig.class})
 public class WebConfig extends WebMvcConfigurerAdapter{
-
+	@Bean
+	public HandlerInterceptor blogInterceptor() {
+		return new BlogInterceptor();
+	}
+	
+	@Override
+	public void addInterceptors(InterceptorRegistry registry) {
+		registry
+			.addInterceptor(blogInterceptor())
+			.addPathPatterns("/**")
+			.excludePathPatterns("/assets/**");
+	}
 }
